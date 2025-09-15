e3f66aed7c27df657d4f46a78e6bfc2c3aaf6e26
Oscar Lara <oscarinlara87@gmail.com>
2025-09-15 17:34:18 -0600

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,18 +1,18 @@
-// --- MÓDULO: ESTADO DEL JUEGO (STATE) ---
+// js/state.js
+// Este módulo centraliza todo el estado mutable del juego.
 export const GameState = {
     currentLevel: 0,
     sanity: 100,
     environment: null,
     filesystem: {},
     awaitingSpecialInput: null,
+    finalSequence: false,
     isTyping: false,
     typingQueue: [],
     recentPlayerInputs: [],
     soundsReady: false,
-    cycle: 1,
-    fragments: [],
-    storyData: [],
-    threeJsAnimationId: null,
-    isEnding: false,
+    cycle: 1, // Ciclo actual del jugador
+    fragmentsFound: 0, // Fragmentos de anulación encontrados
+    story: [], // La narrativa se cargará aquí
 };
 
